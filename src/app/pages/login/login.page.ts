8edd9e8183b548f9f4741e41c3bf6b9d742d7416
kseniyaTurava <xturava@gmail.com>
2021-08-18 15:04:42 +0200

    Login page css UI/UX Design version 0.0.1

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  showMenu = false;
+  constructor() {
+  }
 
   ngOnInit() {
   }
-
+  showSlide(){
+   if(this.showMenu){this.showMenu = false;
+   } else{  this.showMenu = true;}
+   console.log(this.showMenu);
+  }
 }
